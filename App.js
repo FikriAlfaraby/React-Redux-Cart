5dd5a4f03c4aa0740f535773d35b55b1f21bd016
Fikri Alfaraby <100327546+FikriAlfaraby@users.noreply.github.com>
2022-02-26 21:25:23 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ShoppingCart from './ShoppingCart'
+import { Provider } from 'react-redux'
+import store from './store'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+        <ShoppingCart />
+      </Provider>
   );
 }
 
